dce6ade22b8601406807520d2f9c2b568cb14325
Forrest <forrestshi802@gmail.com>
2026-02-15 22:12:05 +0900

    完善course routes

--- server/routes/course-route.js
+++ server/routes/course-route.js
@@ -8,6 +8,62 @@ router.use((req,res,next) => {
 })
 
 
+// 獲得系統中的所有課程
+router.get("/", async (req, res) => {
+  try {
+    let courseFound = await Course.find({})
+      .populate("instructor", ["username", "email"])
+      .exec();
+    return res.send(courseFound);
+  } catch (e) {
+    return res.status(5000).send(e);
+  }
+});
+
+// 用講師id來尋找課程
+router.get("/instructor/:_instructor_id", async (req, res) => {
+  let { _instructor_id } = req.params;
+  let coursesFound = await Course.find({ instructor: _instructor_id })
+    .populate("instructor", ["username", "email"])
+    .exec();
+  return res.send(coursesFound);
+});
+
+// 用學生id來尋找註冊過的課程
+router.get("/student/:_student_id", async (req, res) => {
+  let { _student_id } = req.params;
+  let coursesFound = await Course.find({ students: _student_id })
+    .populate("instructor", ["username", "email"])
+    .exec();
+  return res.send(coursesFound);
+});
+
+// 用課程名稱尋找課程
+router.get("/findByName/:name", async (req, res) => {
+  let { name } = req.params;
+  try {
+    let courseFound = await Course.find({ title: name })
+      .populate("instructor", ["email", "username"])
+      .exec();
+    return res.send(courseFound);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+// 用課程id尋找課程
+router.get("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let courseFound = await Course.findOne({ _id })
+      .populate("instructor", ["email"])
+      .exec();
+    return res.send(courseFound);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
 // 新增課程
 router.post("/", async (req, res) => {
   // 驗證數據符合規範
@@ -23,7 +79,7 @@ router.post("/", async (req, res) => {
   let { title, description, price } = req.body;
   try {
     let newCourse = new Course({
-      title,// 等价于 title: title。当变量名和属性名一样的时候，可以只写一个：
+      title,
       description,
       price,
       instructor: req.user._id,
@@ -32,6 +88,72 @@ router.post("/", async (req, res) => {
     return res.send("新課程已經保存");
   } catch (e) {
     return res.status(500).send("無法創建課程。。。");
+  }
+});
+
+// 讓學生透過課程id來註冊新課程
+router.post("/enroll/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let course = await Course.findOne({ _id }).exec();
+    course.students.push(req.user._id);
+    await course.save();
+    return res.send("註冊完成");
+  } catch (e) {
+    return res.send(e);
+  }
+});
+
+// 更改課程
+router.patch("/:_id", async (req, res) => {
+  // 驗證數據符合規範
+  let { error } = courseValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let { _id } = req.params;
+  // 確認課程存在
+  try {
+    let courseFound = await Course.findOne({ _id });
+    if (!courseFound) {
+      return res.status(400).send("找不到課程。無法更新課程內容。");
+    }
+
+    // 使用者必須是此課程講師，才能編輯課程
+    if (courseFound.instructor.equals(req.user._id)) {
+      let updatedCourse = await Course.findOneAndUpdate({ _id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      return res.send({
+        message: "課程已經被更新成功",
+        updatedCourse,
+      });
+    } else {
+      return res.status(403).send("只有此課程的講師才能編輯課程。");
+    }
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  // 確認課程存在
+  try {
+    let courseFound = await Course.findOne({ _id }).exec();
+    if (!courseFound) {
+      return res.status(400).send("找不到課程。無法刪除課程。");
+    }
+
+    // 使用者必須是此課程講師，才能刪除課程
+    if (courseFound.instructor.equals(req.user._id)) {
+      await Course.deleteOne({ _id }).exec();
+      return res.send("課程已被刪除。");
+    } else {
+      return res.status(403).send("只有此課程的講師才能刪除課程。");
+    }
+  } catch (e) {
+    return res.status(500).send(e);
   }
 });
 
